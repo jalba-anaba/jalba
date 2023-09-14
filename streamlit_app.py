bcdb099003d4084274a31ee6bd8a92a36499c48b
jalba-anaba <144980967+jalba-anaba@users.noreply.github.com>
2023-09-14 04:44:42 +0000

    乱数生成機能を追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,20 +11,20 @@ st.title('gishi')
 user_input = st.text_input('あなたの名前を入力してください')
 
 # ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
+if st.button('周咲です'):
     if user_input:  # 名前が入力されているかチェック
         st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
     else:
         st.error('名前を入力してください。')  # エラーメッセージを表示
 
 # スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
+number = st.slider('好きな数字（8進数）を選んでください', 0, 100)
 
 # 補足メッセージ
 st.caption("十字キー（左右）でも調整できます。")
 
 # 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
+st.write(f'youは「{number}」です。')
 
 # 選択した数値を2進数に変換
 binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く

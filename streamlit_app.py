ef9fa71eb8b2e31553be013d53ebd8e452db82eb
jalba-anaba <144980967+jalba-anaba@users.noreply.github.com>
2023-09-21 04:38:08 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import random 
 st.title("おみくじアプリ")
 if st.button("おみくじを引く"):
-    results=["lucky","civil","matter","negrect","papa","mama","parmanent","yamanohahutoi"]
+    results=["lucky","civil","matter","negrect","papa","mama","parmanent","kisinotinnkoi12sennti"]
     result=random.choice(results)
     st.write(f"結果：{result}")

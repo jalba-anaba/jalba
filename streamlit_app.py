5760221802151896e245b61f711432b82160739f
jalba-anaba <144980967+jalba-anaba@users.noreply.github.com>
2023-09-21 04:37:29 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import random 
 st.title("おみくじアプリ")
 if st.button("おみくじを引く"):
-    results=["lucky","civil","matter","negrect","papa","mama"]
+    results=["lucky","civil","matter","negrect","papa","mama","parmanent","yamanohahutoi"]
     result=random.choice(results)
     st.write(f"結果：{result}")
